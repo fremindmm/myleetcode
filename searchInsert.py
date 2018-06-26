1bf46181aeaab50dd73fa0bba97efa941e57e55d
jeckxie <xiexiaozhe@inspur.com>
2018-06-26 19:49:02 +0800

    implement searchInsert by dichotomy

---
+++ searchInsert.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        index = -1
+        
+        begin = 0
+        
+        end = len(nums) - 1
+        
+        while (index==-1):
+            
+            mid = (begin + end)/2
+            
+            if nums[mid] == target:
+                
+                index= mid
+            
+            elif nums[mid] > target:
+                
+                if mid == 0 or nums[mid-1] < target:
+                    
+                    index = mid
+                end = mid - 1
+            elif nums[mid] < target:
+                
+                if mid == len(nums) -1 or nums[mid+1] > target:
+                    
+                    index = mid + 1
+                
+                begin = mid + 1
+                
+        return index
