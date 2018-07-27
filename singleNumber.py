f6619487c91d76eb65c7d2c68081355bf123cac5
jeckxie <xiexiaozhe@inspur.com>
2018-07-27 19:49:43 +0800

    Given a non-empty array of integers, every element appears twice except for one. Find that single one. Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?

---
+++ singleNumber.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sum = 0
+        for i in nums:
+            sum = sum ^ i
+        return sum       
